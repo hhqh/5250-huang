d847fd3b25e869e9e26b899447d1f46a717104b6
Helen Huang <hhuang1@seattleu.edu>
2022-01-29 17:13:25 -0800

    Added Delete to DatabaseService.cs

--- Mine/Mine/Services/DatabaseService.cs
+++ Mine/Mine/Services/DatabaseService.cs
@@ -83,9 +83,28 @@ namespace Mine.Services
             return true;
         }
 
-        public Task<bool> DeleteAsync(string id)
+        /// <summary>
+        /// Delete data from record
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<bool> DeleteAsync(string id)
         {
-            throw new NotImplementedException();
+            // Check if data exist
+            var data = await ReadAsync(id);
+            if (data == null)
+            {
+                return false;
+            }
+
+            // Delete data
+            var result = await Database.DeleteAsync(data);
+            if (result == 0)
+            {
+                return false;
+            }
+
+            return true;
         }
 
         /// <summary>
